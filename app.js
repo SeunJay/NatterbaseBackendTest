50d7559be8f95c923044fddb827a85d8aa497eb0
seun jay <seunjay92@gmail.com>
2019-10-10 18:25:33 +0100

    export server

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser"); 
-const checkDataRoute = require("./routes/data")
-const removeItemRoute = require("./routes/input")
-
+const bodyParser = require("body-parser");
+const checkDataRoute = require("./routes/data");
+const removeItemRoute = require("./routes/input");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 
-app.use("/api", checkDataRoute)
+app.use("/api", checkDataRoute);
 app.use("/api", removeItemRoute);
 
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to the Natter Base API.");
 });
 
-app.listen(port, ()=>{
-  console.log(`Server up and running on port ${port}`)
-})
+if (process.env.NODE_ENV !== 'test'){
+
+  app.listen(port, () => {
+    console.log(`Server up and running on port ${port}`);
+  });
+}
+
+module.exports = app
