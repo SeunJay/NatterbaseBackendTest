93dc12eb70375154b73659a236f8ef3ba78750d1
seun jay <seunjay92@gmail.com>
2019-10-10 17:31:28 +0100

    import route to validate data

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser"); 
+const checkDataRoute = require("./routes/data")
+
 
 const app = express();
 
@@ -7,6 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 6000;
+
+app.use("/api", checkDataRoute)
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the Natter Base API.");
+});
 
 app.listen(port, ()=>{
   console.log(`Server up and running on port ${port}`)
