de651681bf310f4ef96e809cf79909aac0a76b18
seun jay <seunjay92@gmail.com>
2019-10-11 07:18:48 +0100

    require aladdinRoute

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const checkDataRoute = require("./routes/data");
 const removeItemRoute = require("./routes/input");
+const aladdinRoute = require("./routes/aladdin");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3200;
 
 app.use("/api", checkDataRoute);
 app.use("/api", removeItemRoute);
+app.use("/api", aladdinRoute);
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to the Natter Base API.");
+  res.status(200).send("Hello world, this is the Natter Base API.");
 });
 
 if (process.env.NODE_ENV !== "test") {
